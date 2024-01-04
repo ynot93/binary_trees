6de5cadf0e4ee8e93a31f4101cb75f6ade6b8842
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-05 00:16:59 +0300

    Fix betty errors

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -11,42 +11,41 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-    if (tree == NULL)
-        return (NULL);
+	if (tree == NULL)
+		return (NULL);
 
-    if (*tree == NULL)
-    {
-        *tree = binary_tree_node(NULL, value);
-        return (*tree);
-    }
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
 
-    if (value < (*tree)->n)
-    {
-        if ((*tree)->left == NULL)
-        {
-            (*tree)->left = binary_tree_node(*tree, value);
-            return ((*tree)->left);
-        }
-        else
-        {
-            return (bst_insert(&((*tree)->left), value));
-        }
-    }
-    else if (value > (*tree)->n)
-    {
-        if ((*tree)->right == NULL)
-        {
-            (*tree)->right = binary_tree_node(*tree, value);
-            return ((*tree)->right);
-        }
-        else
-        {
-            return (bst_insert(&((*tree)->right), value));
-        }
-    }
-    else
-    {
-        return (NULL);
-    }
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			return ((*tree)->left);
+		}
+		else
+		{
+			return (bst_insert(&((*tree)->left), value));
+		}
+	}
+	else if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			return ((*tree)->right);
+		}
+		else
+		{
+			return (bst_insert(&((*tree)->right), value));
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
 }
-
--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -10,17 +10,21 @@
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
-    bst_t *root = NULL;
-    size_t i;
+	bst_t *root = NULL;
+	size_t i;
 
-    if (array == NULL || size == 0)
-        return (NULL);
+	if (array == NULL || size == 0)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        if (bst_insert(&root, array[i]) == NULL)
-            return (NULL);
-    }
+	for (i = 0; i < size; i++)
+	{
+		if (bst_insert(&root, array[i] == NULL))
+		{
+			return (NULL);
+		}
+	}
 
-    return (root);
+	return (root);
 }
