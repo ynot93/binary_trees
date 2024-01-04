6e072914e1dd08428e9c639b2da544b8e8fbc620
Tony Mputhia <tonymmputhia@gmail.com>
2024-01-05 00:24:34 +0300

    Fix compilation error

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -20,7 +20,7 @@ bst_t *array_to_bst(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		if (bst_insert(&root, array[i] == NULL))
+		if (bst_insert(&root, array[i]) == NULL)
 		{
 			return (NULL);
 		}
